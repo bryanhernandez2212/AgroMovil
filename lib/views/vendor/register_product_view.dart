bef2d74cb043d8a137a61a54a005dac7ce6e5bfc
bryanhernandez2212 <150862501+bryanhernandez2212@users.noreply.github.com>
2025-11-16 00:34:45 -0600

    Remove unnecessary initialization of ScrollController

--- lib/views/vendor/register_product_view.dart
+++ lib/views/vendor/register_product_view.dart
@@ -41,7 +41,7 @@ class _RegisterProductViewContentState extends State<RegisterProductViewContent>
   bool _isLoadingAllow = false;
   List<String> _categories = [];
   List<String> _units = [];
-  final ScrollController _scrollController = ScrollController();
+  
 
   @override
   void initState() {
